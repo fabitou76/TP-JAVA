0d3f28f0afde8149fb30cc2554b6b0d4d2694c93
Max <mf007@protonmail.com>
2019-04-11 22:39:03 -0400

    XML - exportDataInXML()
    - méthode intégrée à l'interface graphique
    - il manque le choix du dossier de sauvegarde
    - ll faut réduire la taille de la fonction collectData()

--- src/ca/csf/dfc/poo/main/Main.java
+++ src/ca/csf/dfc/poo/main/Main.java
@@ -18,20 +18,22 @@ public class Main {
 
 	public static void main(String[] args) throws XMLStreamException, FactoryConfigurationError {
 		
-		List<Shape> shapes = new ArrayList<Shape>();
-		Point p1 = new Point(200,100);
-		Point p2 = new Point (50,50);
-		Rectangle rect1 = new Rectangle(p1,p2);
-		p1 = new Point(20,20);
-		p2 = new Point(60,55);
-		Line line1 = new Line(p1,p2);
-		shapes.add(rect1);
-		shapes.add(line1);
-		IExportData xmlSaver = new ExportDataInXML(shapes);
-		xmlSaver.collectData();
-		new ImportDataFromXML();
-		
+//		List<Shape> shapes = new ArrayList<Shape>();
+//		Point p1 = new Point(200,100);
+//		Point p2 = new Point (50,50);
+//		Rectangle rect1 = new Rectangle(p1,p2);
+//		p1 = new Point(20,20);
+//		p2 = new Point(60,55);
+//		Line line1 = new Line(p1,p2);
+//		shapes.add(rect1);
+//		shapes.add(line1);
 		new GraphicInterface().setVisible(true);
+		
+//		IExportData xmlSaver = new ExportDataInXML(shapes);
+//		xmlSaver.collectData();
+//		new ImportDataFromXML();
+		
+		
 	}
 
 }
