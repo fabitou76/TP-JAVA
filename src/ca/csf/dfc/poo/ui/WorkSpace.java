f72f34267e68d93de686fba2ffa3e64e17359f46
Hugo <hugo.gelinasmathieu@gmail.com>
2019-04-13 08:12:47 -0800

    nettoyage du code et amélioration du code du workspace

--- src/ca/csf/dfc/poo/ui/WorkSpace.java
+++ src/ca/csf/dfc/poo/ui/WorkSpace.java
@@ -16,21 +16,21 @@ import javax.swing.*;
 
 public class WorkSpace extends JPanel{
 
-	private static final Color DEFAULT_BORDER_COLOR = Color.black;
 	private static final long serialVersionUID = 6100091375438707781L;
+	
+	private static final Color DEFAULT_BORDER_COLOR = Color.black;
+	private static final int DEFAULT_BORDER_WIDTH = 1;
+	private static final String DEFAULT_SHAPE = "elipse";
+	
 	private List<Shape> m_shapeList;
 	private Point m_initialPoint;
 	private Point m_finalPoint;
 	private ShapeFactory m_shapeFactory = new ShapeFactory();
-	private String m_selectedShape = "elipse";
 	
-	private Color m_selectedFillColor = Color.green;
-	private int m_selectedBorderWidth = 7;
-	private Color m_selectedBorderColor = Color.red;
-	
-	
-	
-	
+	private String m_selectedShape = DEFAULT_SHAPE;
+	private int m_selectedBorderWidth = DEFAULT_BORDER_WIDTH;
+	private Color m_selectedBorderColor = DEFAULT_BORDER_COLOR;
+	private Color m_selectedFillColor = null;
 	
 	
     //test
@@ -42,11 +42,10 @@ public class WorkSpace extends JPanel{
     
 	public WorkSpace() {
 		super();
-		//this.setBackground(Color.white);
-		//this.setForeground(Color.BLACK);
+		this.setBackground(Color.white);
 		this.m_shapeList = new ArrayList<Shape>();
+		this.addMouseListener(new MouseHandler());
 		
-      
        //Panel test
         JPanel panelTest = new JPanel();
         panelTest.setLayout(new GridLayout(3,1));
@@ -55,131 +54,26 @@ public class WorkSpace extends JPanel{
         panelTest.add(m_testP2);
         this.add(panelTest, BorderLayout.EAST);
        //fin test
-
-	    this.addMouseListener(new MouseHandler());
-	      
 	}
-	
 	
 	public List<Shape> getShapeList(){
 		return this.m_shapeList;
 	}
 	
+	public void setSelectedFillColor(Color p_color) {
+		this.m_selectedFillColor = p_color;
+	}
+	
+	public void setSelectedBorderWidth(int p_width) {
+		this.m_selectedBorderWidth = p_width;
+	}
+	
+	public void setSelectedBorderColor(Color p_color) {
+		this.m_selectedBorderColor = p_color;
+	}
 	
 	public void setSelectedShape(String p_shapeType) {
 		this.m_selectedShape = p_shapeType;
-	}
-	
-	
-	
-	/*
-	public void addShape(Shape p_shape) {
-		this.m_shapeList.add(p_shape);
-	}
-	*/
-	
-	private void addShapeToWorkSpace() {
-		Shape shape = this.m_shapeFactory.createShape(this.m_selectedShape, this.m_initialPoint, this.m_finalPoint);
-		shape.setFillColor(this.m_selectedFillColor);
-		shape.setBorderWidth(this.m_selectedBorderWidth);
-		shape.setBorderColor(this.m_selectedBorderColor);
-		
-		this.m_shapeList.add(shape);
-		//test
-		System.out.println("--------Début liste workspace---------");
-		for(Shape s: this.m_shapeList) {
-			System.out.println("-"+s.toString());
-			System.out.println(s.getInitialPoint().toString() + "  " + s.getFinalPoint().toString());
-			System.out.println("Hauteur:" + s.getHeight() + " " + "Largeur:" + s.getWidth());
-		}
-		System.out.println("--------Fin liste workspace---------");
-		//fin
-		this.repaint();
-		this.m_initialPoint = null;
-		this.m_finalPoint = null;
-	}
-	
-	
-	@Override
-	protected void paintComponent(Graphics p_Graphic) {
-		super.paintComponent(p_Graphic);
-		Graphics2D g2 = (Graphics2D) p_Graphic;
-		
-		
-		
-		
-		
-		if(!this.m_shapeList.isEmpty()) {
-			
-			for(Shape shape : this.m_shapeList) {
-				g2.setStroke(new BasicStroke(shape.getBorderWidth()));
-				int coordX2, coordY2, width, height,coordX1,coordY1;
-				boolean hasFillColor = false;
-				coordX1 = shape.getInitialPoint().x;
-				coordY1 = shape.getInitialPoint().y;
-				coordX2 = shape.getFinalPoint().x;
-				coordY2 = shape.getFinalPoint().y;
-				height = shape.getHeight();
-				width = shape.getWidth();
-				
-				if(shape.getFillColor() != null) {
-					hasFillColor = true;
-				}
-				
-				switch (shape.getName()) {
-				
-				case "rectangle":
-				
-					
-					
-				
-				
-				
-					
-					
-					if(hasFillColor) {
-						g2.setColor(shape.getFillColor());
-						
-						p_Graphic.fillRect(coordX1,coordY1, width, height);
-					}
-					
-				
-					
-					g2.setColor(shape.getBorderColor());
-					p_Graphic.drawRect(coordX1,coordY1, width, height);
-					
-	
-					
-					break;
-				case "line":
-					
-					if(hasFillColor) {
-						g2.setColor(shape.getFillColor());
-					}
-					
-					p_Graphic.drawLine(coordX1, coordY1, coordX2, coordY2);
-				
-					break;
-				case "elipse":
-				
-			
-					
-					if(hasFillColor){
-						g2.setColor(shape.getFillColor());
-						p_Graphic.fillOval(coordX1,coordY1, width, height);
-					}
-					g2.setColor(shape.getBorderColor());
-					p_Graphic.drawOval(coordX1,coordY1, width, height);
-					
-					
-				default:
-					break;
-					
-				
-				}
-			}
-		}
-		
 	}
 	
 	private class MouseHandler implements MouseListener{
@@ -204,27 +98,28 @@ public class WorkSpace extends JPanel{
 
 		@Override
 		public void mousePressed(MouseEvent p_arg0) {
-			Point point = new Point();
+		
 			if(WorkSpace.this.m_initialPoint == null) {
+				Point point = new Point();
 				point.x = p_arg0.getX();
 				point.y = p_arg0.getY();
 				WorkSpace.this.m_initialPoint = point;
-				WorkSpace.this.m_testP1.setText(WorkSpace.this.m_initialPoint.toString());
-				
+				WorkSpace.this.m_testP1.setText(WorkSpace.this.m_initialPoint.toString()); //test
 			}
 			else if (WorkSpace.this.m_finalPoint == null) {
+				Point point = new Point();
 				point.x = p_arg0.getX();
 				point.y = p_arg0.getY();
 				WorkSpace.this.m_finalPoint = point;
-				WorkSpace.this.m_testP2.setText(WorkSpace.this.m_finalPoint.toString());
+				WorkSpace.this.m_testP2.setText(WorkSpace.this.m_finalPoint.toString()); //test
 			}
 			
-			if(WorkSpace.this.m_initialPoint != null && WorkSpace.this.m_finalPoint != null) {
-				WorkSpace.this.addShapeToWorkSpace();
-				//FenetrePrincipal.this.drawShapes();
-				
+			if(WorkSpace.this.coordinatesAreSet()) {
+				Shape newShape = WorkSpace.this.createShape();
+				WorkSpace.this.addShapeToWorkSpace(newShape);
+				WorkSpace.this.reloadWorkSpace();
+				WorkSpace.this.resetCoordinates();
 			}
-			
 		}
 
 		@Override
@@ -233,6 +128,105 @@ public class WorkSpace extends JPanel{
 			
 		}
 	}
+	
+	private boolean coordinatesAreSet() {
+		boolean coordsAreSet = false;
+		if(WorkSpace.this.m_initialPoint != null && WorkSpace.this.m_finalPoint != null) {
+			coordsAreSet = true;
+		}
+		return coordsAreSet;
+	}
 
+	private Shape createShape() {
+		Shape newShape = this.m_shapeFactory.createShape(this.m_selectedShape, this.m_initialPoint, this.m_finalPoint);
+		newShape.setFillColor(this.m_selectedFillColor);
+		newShape.setBorderWidth(this.m_selectedBorderWidth);
+		newShape.setBorderColor(this.m_selectedBorderColor);
+		
+		return newShape;
+	}
+	
+	private void addShapeToWorkSpace(Shape p_shape) {
+		this.m_shapeList.add(p_shape);
+		//test
+		System.out.println("--------Début liste workspace---------");
+		for(Shape s: this.m_shapeList) {
+			System.out.println("-"+s.toString());
+			System.out.println(s.getInitialPoint().toString() + "  " + s.getFinalPoint().toString());
+			System.out.println("Hauteur:" + s.getHeight() + " " + "Largeur:" + s.getWidth());
+		}
+		System.out.println("--------Fin liste workspace---------");
+		//fin
+	}
+	
+	private void reloadWorkSpace() {
+		this.repaint();
+	}
+	
+	private void resetCoordinates(){
+		this.m_initialPoint = null;
+		this.m_finalPoint = null;
+	}
+	
+	@Override
+	protected void paintComponent(Graphics p_Graphic) {
+		
+		super.paintComponent(p_Graphic);
+		Graphics2D g2 = (Graphics2D) p_Graphic;
+		
+		if(!this.m_shapeList.isEmpty() && this.m_shapeList != null) {
+			
+			for(Shape shape : this.m_shapeList) {
+				
+				g2.setStroke(new BasicStroke(shape.getBorderWidth()));
+				int coordX2, coordY2, width, height,coordX1,coordY1;
+				
+				coordX1 = shape.getInitialPoint().x;
+				coordY1 = shape.getInitialPoint().y;
+	
+				height = shape.getHeight();
+				width = shape.getWidth();
+				
+				boolean shapeHasFillColor = false;
+				if(shape.getFillColor() != null) {
+					shapeHasFillColor = true;
+				}
+				
+				switch (shape.getName()) {
+				
+					case "rectangle":
+						if(shapeHasFillColor) {
+							g2.setColor(shape.getFillColor());
+							p_Graphic.fillRect(coordX1,coordY1, width, height);
+						}
+						g2.setColor(shape.getBorderColor());
+						p_Graphic.drawRect(coordX1,coordY1, width, height);
+						break;
+						
+					case "line":
+						coordX2 = shape.getFinalPoint().x;
+						coordY2 = shape.getFinalPoint().y;
+						if(shapeHasFillColor) {
+							g2.setColor(shape.getFillColor());
+						}
+						p_Graphic.drawLine(coordX1, coordY1, coordX2, coordY2);
+						break;
+						
+					case "elipse":
+						if(shapeHasFillColor){
+							g2.setColor(shape.getFillColor());
+							p_Graphic.fillOval(coordX1,coordY1, width, height);
+						}
+						g2.setColor(shape.getBorderColor());
+						p_Graphic.drawOval(coordX1,coordY1, width, height);
+						
+					default:
+						break;
+					
+				}
+			}
+		}
+	}
+	
 	
 }
