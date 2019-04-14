4c546fdae7e70910bd358a043affb401387c9e0a
Fabio <fabitou@gmail.com>
2019-04-14 18:46:26 -0400

    Add classes and start tests

---
+++ src/ca/csf/dfc/test/ElipseTest.java
@@ -0,0 +1,33 @@
+package ca.csf.dfc.test;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ElipseTest {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+	
+	public void ElipseTest() {
+		
+	}
+	
+		
+	@Before
+	public void before() {
+		System.out.println("Début du test");
+	}
+	
+	@After
+	public void after() {
+		System.out.println("Fin du test");
+	}
+
+}
