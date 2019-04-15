7412583802a78c3f94cbc817b98abe8a471b67e0
Lau <laurence_lambert_99@hotmail.com>
2019-04-15 08:40:31 -0400

    Visuel

--- src/ca/csf/dfc/poo/ui/GraphicInterface.java
+++ src/ca/csf/dfc/poo/ui/GraphicInterface.java
@@ -15,48 +15,62 @@ import javax.swing.border.LineBorder;
 
 import com.sun.prism.paint.Color;
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import ca.csf.dfc.poo.classes.*;
 
 public class  GraphicInterface extends JFrame{
 
 	private static final long serialVersionUID = 1L;
-=======
+//=======
 //
 
-public class  GraphicInterface extends JFrame{
-
 	// TEST POUR HUGO
->>>>>>> master
+//>>>>>>> master
 	private WorkSpace m_workSpace = new WorkSpace();
 	
-	JButton m_btn_rectangle = new JButton("Rectangle");
-    JButton m_btn_elipse = new JButton("Elipse");
-    JButton m_btn_line = new JButton("Ligne");
+	JButton m_btn_rectangle = new JButton(new ImageIcon("src/rectangle.png"));
+    JButton m_btn_elipse = new JButton(new ImageIcon("src/ellipse.png"));
+    JButton m_btn_line = new JButton(new ImageIcon("src/line.png"));
     JButton m_btn_clear = new JButton("Clear");
+    
+    String[] m_ColorChoice = {"Aucune", "Blanc", "Noir", "Bleu", "Rouge", "Jaune", "Vert", "Orange", "Mauve"};
+    Double[] m_BorderWidthOptions = {0.5, 1.0, 1.5, 2.0};
+    
+    
+    JComboBox m_BorderColor = new JComboBox(m_ColorChoice);
+    JComboBox m_FillColor = new JComboBox(m_ColorChoice);
+    JComboBox m_BorderWidth = new JComboBox(m_BorderWidthOptions);
+    
+    JMenuBar menuBar = new JMenuBar();
+    
+    JMenu m_newDrawing = new JMenu("Nouveau dessin");
+	JMenu m_loadXML = new JMenu("Charger");
+	JMenu m_saveXML = new JMenu("Sauvegarder");
+	JMenu m_exportSVG = new JMenu("Exporter");
+    
     
 
 	public GraphicInterface() {
-  	  super("FenetrePrincipale");
+  	  super("Dessin vectoriel");
+  	  
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       this.setLayout(new BorderLayout());
-    
+      this.setSize(800, 400);
+      this.setLocationRelativeTo(null);
 
-      this.add(this.m_workSpace, BorderLayout.CENTER);
-
-      
       this.initMenu();
       this.initShapeGrid();
       
-      this.setSize(800, 400);
-      this.setLocationRelativeTo(null);
+      this.add(this.m_workSpace, BorderLayout.CENTER);
+      
+  
       
 	}
 	
 	
 	private void initShapeGrid() {
 		JPanel panneauForme = new JPanel();
-		panneauForme.setLayout(new GridLayout(3,1));
+		panneauForme.setLayout(new GridLayout(1,10));
 		
 		this.m_btn_elipse.addActionListener(new ShapeBtnHandler());
 		this.m_btn_line.addActionListener(new ShapeBtnHandler());
@@ -68,16 +82,22 @@ public class  GraphicInterface extends JFrame{
         panneauForme.add(this.m_btn_line);
         panneauForme.add(this.m_btn_clear);
         
+        panneauForme.add(this.m_BorderColor);
+        panneauForme.add(this.m_FillColor);
+        panneauForme.add(this.m_BorderWidth);
     
-        this.add(panneauForme, BorderLayout.WEST);
+        this.add(panneauForme, BorderLayout.NORTH);
         
 	}
-	
+
 	
 	private void initMenu() {
-		JMenuBar menuBar = new JMenuBar();
-		JMenu menuFichier = new JMenu("Fichier");
-		menuBar.add(menuFichier);
+		
+		menuBar.add(m_newDrawing);
+		menuBar.add(m_loadXML);
+		menuBar.add(m_saveXML);
+		menuBar.add(m_exportSVG);
+		
 		this.setJMenuBar(menuBar);
 		
 	}
@@ -114,19 +134,43 @@ public class  GraphicInterface extends JFrame{
 			GraphicInterface.this.m_workSpace.getShapeList().clear();
 			GraphicInterface.this.m_workSpace.repaint();
 		}
-
-		
-		
-		
 		
 	}
 	
+	public Color getFillColor(){
 		
-	
-	
-	
-	
+	    String[] m_ColorChoice = {"Aucune", "Blanc", "Noir", "Bleu", "Rouge", "Jaune", "Vert", "Orange", "Mauve"};
+
+		int selectedColor = this.m_FillColor.getSelectedIndex();
+		
+		Color chosenColor = Color.TRANSPARENT;
+		
+		Switch (selectedColor){
+			case 1:
+				color = Color.WHITE;
+				break;
+			case 2:
+				color = Color.BLACK;
+				break;
+			default:
+				color = Color.TRANSPARENT;
+				break;
+			
+			
+			
+		}
+		
+		
+		
+		
+		return Color.BLACK;
+	}
+
+
+	private void Switch(Object selectedColor) {
+		// TODO Auto-generated method stub
+		
+	}
 }
-	
 	
 
